ef47ea2cf2f1bb200817d08c367cc670de1c960c
mghaddaf <mghaddaf@gmail.com>
2026-04-16 20:15:32 +0100

    Add files via upload

---
+++ multiplication table generator.py
@@ -0,0 +1,46 @@
+import tkinter, tkinter.ttk
+
+screen = tkinter.Tk()
+screen.geometry("300x700")
+screen.title("Multiplication table")
+def GenerateTable():
+    number = int(Combobox1.get())
+    for i in range(x_rate):
+        equation = number + "X" + i + "=" + number * i
+        i = i + 1
+    Label3.config(text = equation)
+Label1 = tkinter.Label(screen, text = "Mathematical Table")
+Label2 = tkinter.Label(screen, text = "Number and Range: ")
+Label3 = tkinter.Label(screen, text = "")
+Button1 = tkinter.Button(screen, text = "Generate")
+Combobox1 = tkinter.ttk.Combobox(screen)
+Numbers = []
+for i in range(101):
+    Numbers.append(i)
+Combobox1["values"] = Numbers
+
+x_rate = tkinter.IntVar()
+Radiobutton1 = tkinter.ttk.Radiobutton(screen, text = "10", variable=x_rate, value=10)
+Radiobutton2 = tkinter.ttk.Radiobutton(screen, text = "20", variable=x_rate, value=20)
+Radiobutton3 = tkinter.ttk.Radiobutton(screen, text = "30", variable=x_rate, value=30)
+Label1.grid(row = 1, column = 2)
+Label2.grid(row = 2, column = 1)
+Label3.grid(row = 3, column = 2)
+Button1.grid(row = 5, column = 2)
+Combobox1.grid(row = 2, column = 2)
+Radiobutton1.grid(row = 2, column = 3)
+Radiobutton2.grid(row = 3,column = 3)
+Radiobutton3.grid(row = 4, column = 3)
+
+
+
+
+
+
+
+
+
+
+
+
+screen.mainloop()
