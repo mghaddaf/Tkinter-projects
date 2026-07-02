529dc621a811cb9857368179e8a1d00def62025a
mghaddaf <mghaddaf@gmail.com>
2026-07-02 20:10:05 +0100

    Add files via upload

--- Address Book.py
+++ Address Book.py
@@ -1,7 +1,7 @@
 import tkinter, tkinter.filedialog
 screen = tkinter.Tk()
 screen.geometry("500x500")
-screen.title("Adress book")
+screen.title("Address book")
 data = {}
 def OpenButton():
     global data
@@ -15,6 +15,9 @@ def AddButton():
     Mobile = Entry3.get()
     Email = Entry4.get()
     Birthday = Entry5.get()
+    if Name == "":
+        tkinter.messagebox.showinfo("Invalid!", "Invalid data, please fill in all boxes with data")
+        return
     data[Name] = [Address, Mobile, Email, Birthday]
     UpdateListBox()
     Entry1.delete(0, tkinter.END)
@@ -23,18 +26,27 @@ def AddButton():
     Entry4.delete(0, tkinter.END)
     Entry5.delete(0, tkinter.END)
 
+
 def UpdateListBox():
         Listbox1.delete(0, tkinter.END)
         for name in data.keys():
             Listbox1.insert(tkinter.END, name)
 
 def DeleteButton():
-    item = Listbox1.curselection()
-    Listbox1.delete(item)
+    index = Listbox1.curselection() 
+    name = Listbox1.get(index)
+    del data[name]
+    UpdateListBox()
 def SaveButton():
     File1 = tkinter.filedialog.asksaveasfile()
     if File1 != None:
         print(data, file = File1)
+
+def view(event):
+    namedata = Listbox1.get(Listbox1.curselection())
+    info = data[namedata]
+    tkinter.messagebox.showinfo("Data", info)
+
 
 Label1 = tkinter.Label(screen, text = "My address book")
 Label2 = tkinter.Label(screen, text = "Name:")
@@ -53,6 +65,7 @@ Entry3 = tkinter.Entry(screen)
 Entry4 = tkinter.Entry(screen)
 Entry5 = tkinter.Entry(screen)
 Listbox1 = tkinter.Listbox(screen)
+Listbox1.bind("<<ListboxSelect>>", view)
 Label1.grid(row = 2, column = 2)
 Label2.grid(row = 2, column = 3)
 Label3.grid(row = 3, column = 3)
